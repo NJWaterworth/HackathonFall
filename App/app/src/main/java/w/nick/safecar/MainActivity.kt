8345076ce4ef409e4a4488750372346c2939b269
Jasper Harrison <jasperharison@gmail.com>
2018-09-21 20:46:15 -0500

    git commit#

--- App/app/src/main/java/w/nick/safecar/MainActivity.kt
+++ App/app/src/main/java/w/nick/safecar/MainActivity.kt
@@ -2,11 +2,102 @@ package w.nick.safecar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.sample.basiclocationsample.BuildConfig.APPLICATION_ID
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = 'MainActivity'
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var latitudeText: TextView
+    private lateinit var longitudeText: TexView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        latitudeText = findViewById(R.id.latitude_text)
+        longitudeText = findViewById(R.id.longitude_text)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(!checkPermissions()) {
+            requestPermissions()
+        } else {
+            getLastLocation()
+        }
+    }
+
+    @SuppressLint("Missing Permissions")
+    private fun getLastLocation() {
+        fusedLocationClient.lastLocation.addOnCompleteListener(this) {
+            task -> if(task.isSuccessful && task.result != null) {
+                latitudeText.text = resources.getString(R.string.latitude_label, task.result.latitude)
+                longitudeText.text = resources.getString(R.string.longitude_label, task.result.longitude)
+            else {
+                Log.w(TAG, "getLastLocation:exception", task.exception)
+                showSnackbar(R.string.no_location_detected)
+            }
+        }
+        }
+    }
+
+    private fun showSnackbar(
+        snackStrId: Int,
+        actionStrId: Int = 0,
+        listener: View.OnClickListener? = null
+    ) {
+        val snackbar = Snackbar.make(findViewById(android.R.id.content), getString(snackStrid), LENGTH_INDEFINITE)
+        if(actionStrId != - && listener != null) {
+            snackbar.setAction(getString(actionStrId), listener)
+        }
+        snackbar.show()
+    }
+
+    private fun checkPermissions() = ActivityCompat.checkSelfPermissions(this, ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED
+
+    private fun startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(this, arrayOf(ACCESS_COARSE_LOCATION), REQUEST_PERMISSIONS_REQUEST_CODE)
+    }
+
+    private fun requestPermissions() {
+        if(AcivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_COARSE_LOCATION)) {
+            Log.i(TAG, "Diplaying permission rationale to provide additional context")
+            showSnackbar(R.string.permission_rationale, android.R.string.ok, View.OnClickListener {
+                startLocationPermissionRequest()
+            })
+        }
+        else {
+            Log.i(TAG, "Requesting Permission")
+            startLocationPermissionRequest()
+        }
+    }
+
+    override fun onRequestPermissionResult (
+            requestCode: Int,
+            permissions: Array<String>,
+            grantResults: IntArray
+    ) {
+        Log.i(TAG, "onRequestPermissionResult")
+        if(requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            when {
+                grantResults.isEmpty() -> Log.i(TAG, "User interaction was cancelled")
+
+                (grantResults[0] == PackageManager.PERMISSION_GRANTED) -> getLastLocation()
+
+                else -> {
+                    showSnackbar(R.string.permission_denied_explanation, R.string.settings, View.OnClickListener {
+                        val intent = Intent().apply {
+                            action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            data = Uri.fromParts("package", APPLICATION_ID, null)
+                            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        }
+                        startActvity(intent)
+                    })
+                }
+            }
+        }
     }
 }
