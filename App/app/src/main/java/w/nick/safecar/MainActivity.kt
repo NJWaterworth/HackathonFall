d8816dd1f86db5eba9775a4db5c6bf98cc57c7a4
Jkharrison <32682899+Jkharrison@users.noreply.github.com>
2018-09-21 22:43:39 -0500

    *Note*
    Once added to master branch, will need to update id names and fields as necessar

--- App/app/src/main/java/w/nick/safecar/MainActivity.kt
+++ App/app/src/main/java/w/nick/safecar/MainActivity.kt
@@ -1,23 +1,39 @@
-package w.nick.safecar
+package com.example.jasperharrison.locationtest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.Uri
+import android.provider.Settings
+import android.support.design.widget.Snackbar
+import android.support.design.widget.Snackbar.LENGTH_INDEFINITE
+import android.support.v4.app.ActivityCompat
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.sample.basiclocationsample.BuildConfig.APPLICATION_ID
+import com.example.jasperharrison.locationtest.BuildConfig.APPLICATION_ID
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = 'MainActivity'
+    private val TAG = "MainActivity"
     private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var latitudeText: TextView
-    private lateinit var longitudeText: TexView
+    private lateinit var longitudeText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        latitudeText = findViewById(R.id.latitude_text)
-        longitudeText = findViewById(R.id.longitude_text)
+
+        latitudeText = findViewById(R.id.latitude_text) as TextView
+        longitudeText = findViewById(R.id.longitude_text) as TextView
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
@@ -30,40 +46,39 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("Missing Permissions")
+    @SuppressLint("MissingPermission")
     private fun getLastLocation() {
-        fusedLocationClient.lastLocation.addOnCompleteListener(this) {
-            task -> if(task.isSuccessful && task.result != null) {
+        fusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+            if (task.isSuccessful && task.result != null) {
                 latitudeText.text = resources.getString(R.string.latitude_label, task.result.latitude)
                 longitudeText.text = resources.getString(R.string.longitude_label, task.result.longitude)
-            else {
+            } else {
                 Log.w(TAG, "getLastLocation:exception", task.exception)
                 showSnackbar(R.string.no_location_detected)
             }
         }
-        }
     }
 
     private fun showSnackbar(
-        snackStrId: Int,
-        actionStrId: Int = 0,
-        listener: View.OnClickListener? = null
+            snackStrId: Int,
+            actionStrId: Int = 0,
+            listener: View.OnClickListener? = null
     ) {
-        val snackbar = Snackbar.make(findViewById(android.R.id.content), getString(snackStrid), LENGTH_INDEFINITE)
-        if(actionStrId != - && listener != null) {
+        val snackbar = Snackbar.make(findViewById(android.R.id.content), getString(snackStrId), LENGTH_INDEFINITE)
+        if(actionStrId != 0 && listener != null) {
             snackbar.setAction(getString(actionStrId), listener)
         }
         snackbar.show()
     }
 
-    private fun checkPermissions() = ActivityCompat.checkSelfPermissions(this, ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED
+    private fun checkPermissions() = ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED
 
     private fun startLocationPermissionRequest() {
         ActivityCompat.requestPermissions(this, arrayOf(ACCESS_COARSE_LOCATION), REQUEST_PERMISSIONS_REQUEST_CODE)
     }
 
     private fun requestPermissions() {
-        if(AcivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_COARSE_LOCATION)) {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_COARSE_LOCATION)) {
             Log.i(TAG, "Diplaying permission rationale to provide additional context")
             showSnackbar(R.string.permission_rationale, android.R.string.ok, View.OnClickListener {
                 startLocationPermissionRequest()
@@ -75,7 +90,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onRequestPermissionResult (
+    private fun onRequestPermissionResult (
             requestCode: Int,
             permissions: Array<String>,
             grantResults: IntArray
@@ -94,10 +109,11 @@ class MainActivity : AppCompatActivity() {
                             data = Uri.fromParts("package", APPLICATION_ID, null)
                             flags = Intent.FLAG_ACTIVITY_NEW_TASK
                         }
-                        startActvity(intent)
+                        startActivity(intent)
                     })
                 }
             }
         }
     }
 }
+
